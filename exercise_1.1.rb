4d25f09c292cc39215b28c830921c8ef7dc9a330
pklein2022 <111322242+pklein2022@users.noreply.github.com>
2023-01-09 20:17:55 -0600

    Ruby Class 1 Exercises

---
+++ exercise_1.1.rb
@@ -0,0 +1,10 @@
+p "p"
+puts "puts"
+print "print"
+
+my_name = "Pam Klein"
+puts #my_name
+
+beginning_sentence = "My name is "
+full_sentence = beginning_sentence + my_name
+puts full_sentence
---
+++ exercise_1.3.rb
@@ -0,0 +1,5 @@
+# Use gets.chomp to get the user input, then store it in a variable called user_input. 
+#Print into the console "You typed: " follow by what the user entered.
+puts "Enter some input here:"
+user_input = gets.chomp
+puts "You typed #{user_input}"
